e3d21d3c1d2ce9f988dd3905440d2ea3c6b65782
Daukaevt <mupbam1@gmail.com>
2025-07-24 22:33:03 +0300

    Initial commit: Eureka Server

---
+++ accaunt-manag9-eureka-server/src/main/java/com/wixsite/mupbam1/AccauntManag9EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.wixsite.mupbam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AccauntManag9EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccauntManag9EurekaServerApplication.class, args);
+	}
+
+}
