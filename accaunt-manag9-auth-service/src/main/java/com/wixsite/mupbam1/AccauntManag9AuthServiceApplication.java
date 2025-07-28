a87a09803f7adee16af85054e9397c2a5c8ddb1a
Daukaevt <mupbam1@gmail.com>
2025-07-28 22:58:57 +0300

    Add accaunt-manag9-auth-service base structure

---
+++ accaunt-manag9-auth-service/src/main/java/com/wixsite/mupbam1/AccauntManag9AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wixsite.mupbam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccauntManag9AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccauntManag9AuthServiceApplication.class, args);
+	}
+
+}
